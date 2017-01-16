0ae5a997387ad3fda58b3475aeca6420f8202502
Pedro Ferreira <plferreira4@gmail.com>
2017-01-16 12:56:36 +0000

    - specify the minor versions that are being used

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "blog_web_api",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1, minor: 4),
     ]
 )
