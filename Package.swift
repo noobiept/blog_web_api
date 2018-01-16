cdf6168dc7017d15bded02e23e347b46a2f626a5
Pedro Ferreira <plferreira4@gmail.com>
2018-01-16 23:07:17 +0000

    - start using swift 4 - update the package file to the new format - update the dependencies to the latest version (to support swift 4) - remove the package pins file and add the resolved one

--- Package.swift
+++ Package.swift
@@ -1,11 +1,29 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "blog_web_api",
+    products: [
+        .executable( name: "blog_web_api", targets: [ "blog_web_api" ] )
+    ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/czechboy0/Redbird.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 8),
+        .package( url: "https://github.com/IBM-Swift/Kitura.git",       .upToNextMajor( from: "2.1.0" ) ),
+        .package( url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMajor( from: "1.7.0" ) ),
+        .package( url: "https://github.com/IBM-Swift/BlueCryptor.git",  .upToNextMajor( from: "0.8.0" ) ),
+        .package( url: "https://github.com/IBM-Swift/SwiftyJSON.git",   .upToNextMajor( from: "17.0.0" ) ),
+        .package( url: "https://github.com/vapor/redis.git",            .upToNextMajor( from: "2.2.0" ) ),
+    ],
+    targets: [
+        .target(
+            name: "blog_web_api",
+            dependencies: [
+                "Kitura",
+                "HeliumLogger",
+                "Redis",
+                "Cryptor",
+                "SwiftyJSON"
+            ],
+            path: "./Sources"
+        )
     ]
 )
